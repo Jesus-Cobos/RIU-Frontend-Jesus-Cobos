9d88d524b77a58e60fa0439754918b075387df48
Cobosito <cobos.contacto@gmail.com>
2025-06-06 17:41:58 +0100

    upload complete project

---
+++ src/app/mocks/super-heroes.mock.ts
@@ -0,0 +1,64 @@
+import { SuperHeroes } from "../features/models/super-heroes.model";
+
+export const mockSuperHeroes: SuperHeroes[] = [
+    {
+        id: 1,
+        name: 'Shadowstrike'
+    },
+    {
+        id: 2,
+        name: 'Titanium Heart'
+    },
+    {
+        id: 3,
+        name: 'Stormbringer'
+    },
+    {
+        id: 4,
+        name: 'Liberty Star'
+    },
+    {
+        id: 5,
+        name: 'Crimson Phantom'
+    },
+    {
+        id: 6,
+        name: 'Jade Titan'
+    },
+    {
+        id: 7,
+        name: 'Quantum Beetle'
+    },
+    {
+        id: 8,
+        name: 'Mystic Sage'
+    },
+    {
+        id: 9,
+        name: 'Golden Leopard'
+    },
+    {
+        id: 10,
+        name: 'Nebula Witch'
+    },
+    {
+        id: 11,
+        name: 'Aether Sentinel'
+    },
+    {
+        id: 12,
+        name: 'Eagle Eye'
+    },
+    {
+        id: 13,
+        name: 'Super-Man'
+    },
+    {
+        id: 14,
+        name: 'Spider-Man'
+    },
+    {
+        id: 15,
+        name: 'Silk Specter'
+    }
+];
